4eca6573e7c8f671321c3a8f587d4e6b6fab1946
Shashidharreddy206 <shashireddy206@gmail.com>
2026-06-18 11:51:08 +0000

    worked forget and signup and otp

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -75,7 +75,7 @@ export function Signup() {
     setApiError('');
     setLoading(true);
     try {
-      const res = await fetch('http://127.0.0.1:8001/api/v1/auth/register', {
+      const res = await fetch('http://127.0.0.1:8000/api/v1/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
         body: JSON.stringify({
